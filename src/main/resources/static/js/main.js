7267dc20435ec1e5bd0cb975c97ba79c07fd320a
HKUMAS <hsnkms75@gmail.com>
2020-02-27 23:41:43 +0100

    Frontend assignment JQuery POST

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -19,6 +19,7 @@ function displayTeachers(teachers) {
         teacher.name + '</td></tr>');
      });
 }
+
 function displayCourses(courses) {
      var teacherCourseContainer = $('#teacherCourseContainer');
      teacherCourseContainer.empty();
@@ -29,7 +30,88 @@ function displayCourses(courses) {
      });
 }
 
+function postTeacher(teacher){
+    var jsonTeacher = JSON.stringify(teacher);
+    $.ajax({
+        url: 'api/teachers',
+        type: 'POST',
+        contentType: 'application/json',
+        data: jsonTeacher,
+        success: function() {
+            alert('We created a new teacher..');
+        },
+        error: function() {
+            alert('Something went wrong..');
+        }
+    });
+}
+
+function createTeacher() {
+    var teacherName = $('#teacherNameInput').val();
+    if (!teacherName) {
+        alert('The teacher name should be set..');
+        return;
+    }
+    if (teacherName.length < 3) {
+        alert('Name is too short..');
+        return;
+    }
+    var teacher = {
+        name: teacherName
+    };
+    postTeacher(teacher);
+}
+
+function createCourse() {
+    var courseName = $('#courseNameInput').val();
+    if (!courseName) {
+        alert('The course name should be set..');
+        return;
+    }
+    if (courseName.length < 3) {
+        alert('Course name is too short..');
+        return;
+    }
+    var course = {
+            teacher: {
+                id: $('#teacherSelect').val()
+            },
+            name: courseName
+    };
+    postCourse(course);
+}
+
+function postCourse(course){
+    var jsonCourse = JSON.stringify(course);
+    $.ajax({
+        url: 'api/courses',
+        type: 'POST',
+        contentType: 'application/json',
+        data: jsonCourse,
+        success: function() {
+            alert('We created a new course..');
+            getCourses()
+        },
+        error: function() {
+            alert('Something went wrong..');
+        }
+    });
+}
+
+function selectTeachers() {
+    $.get('api/teachers', function(teachers){
+           $.each(teachers, function(index, teacher) {
+                $('#teacherSelect').append('<option value = " ' + teacher.id + ' " >' +
+                                                    teacher.name + '</option>');
+           });
+    });
+
+}
+
 $(document).ready(function () {
        $("#getTeachersButton").click(getTeachers);
        $("#getCoursesButton").click(getCourses);
+       $("#createTeacherButton").click(createTeacher);
+       $("#createCourseButton").click(createCourse);
+       selectTeachers();
 });
